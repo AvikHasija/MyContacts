5e72e368686f877cd81977763383fb03bee0cb1f
AvikHasija <avikhasija@gmail.com>
2015-07-17 18:20:12 -0400

    first commit

---
+++ app/src/main/java/com/example/avikhasija/mycontacts/Contact.java
@@ -0,0 +1,17 @@
+package com.example.avikhasija.mycontacts;
+
+/**
+ * Created by Avikshit on 7/17/2015.
+ */
+public class Contact {
+
+    private String mName;
+
+    public String getName() {
+        return mName;
+    }
+
+    public void setName(String name) {
+        mName = name;
+    }
+}
