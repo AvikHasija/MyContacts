325d217f03ebb910440188ad7f353deb46b69875
AvikHasija <avikhasija@gmail.com>
2015-07-17 23:55:05 -0400

    Text fixed, ActionBar removed on second activity
    Status bar also translucent on devices with API 19 and higher

--- app/src/main/java/com/example/avikhasija/mycontacts/ContactViewActivity.java
+++ app/src/main/java/com/example/avikhasija/mycontacts/ContactViewActivity.java
@@ -1,9 +1,13 @@
 package com.example.avikhasija.mycontacts;
 
+import android.graphics.Point;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.Display;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 
@@ -15,6 +19,17 @@ public class ContactViewActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_view);
+
+        Display display = getWindowManager().getDefaultDisplay();
+        Point point = new Point();
+        display.getSize(point);
+
+        int width = point.x;
+        int height = point.y;
+
+        RelativeLayout header = (RelativeLayout) findViewById(R.id.contact_view_header);
+        header.setLayoutParams(new RelativeLayout.LayoutParams(width, (int) (width * (9.0/16.0))));
+        //instead of passing in height variable, pass in terms of width for ratio
 
         Contact contact = (Contact) getIntent().getSerializableExtra(EXTRA);
         TextView contactName = (TextView)findViewById(R.id.contact_view_name);
